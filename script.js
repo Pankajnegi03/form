05f1e83c6113390edf236508887624c5603cfeea
Highrider15 <gautam.chopra23@gmail.com>
2024-01-25 23:37:34 +0530

    fixed

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-
-document.getElementById('myForm').addEventListener('submit', function(e){
-  e.preventDefault();
-
-  const Username = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  if(isEmpty(Username)){
-      window.alert('Name cannot be empty');
-      return;
-  }
-
-  if(!isValidEmail(email)){
-      window.alert('Please enter email in valid format');
-      return;
-  }
-
-  // if(!isValidPassword(password)){
-  //     window.alert('password msust be at least 8 characters long,must have at least 1 alphabet and at least one special character');
-  //     return;
-  // }
-
-  alert('Form sbumitted successfully');
-  
-});
-
 function isEmpty(str){
   if(str === '' || str === null)
       return true;
@@ -33,3 +6,35 @@ function isEmpty(str){
 function isValidEmail(email){
   return email.includes('@') && email.includes('.');
 }
+
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById("myForm");
+
+email.addEventListener("focus",function(e){
+
+  if(username.value.length == 0){
+    window.alert("username cannot be empty!!!");
+    email.blur();
+  }
+
+},true);
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  if(isEmpty(username.value)){
+      window.alert('Name cannot be empty');
+      return;
+  }
+
+  if(!isValidEmail(email.value)){
+      window.alert('Please enter email in valid format');
+      return;
+  }
+
+  alert('Form sumitted successfully');
+  
+});
+
